8e06dc1f2bc7c6499370341a16280cad13f81f21
Scott Dowling <spdowling@gmail.com>
2020-02-04 21:31:21 -0500

    Added individual notebooks for contributors

---
+++ summary findings.py
@@ -0,0 +1,14 @@
+#does teacher salary affect US graduation rate?
+#	yes, there is a correlation betwween these two items
+
+#do financials affect us graduation rate? 
+#	yes there is a correlation between financials and graduation rate
+
+#does per student spending affect us graduation rate?
+#	yes there is a correlation between per student spending and graduation rate
+
+#due to these findings, we believe that if schools were granted more money, graduation rates 
+#would further improve.
+
+#we also believe that GA graduation rate would come more in line with US rates if GA paid
+#their teachers more, and spent more money per student
